159ef71b9d75e1472a2f35d05d93987a2b87942a
Anandhu Manoj <anandhumanoj282@gmail.com>
2020-07-19 22:06:18 +0530

    [new] UI revamp

--- js/main.js
+++
@@ -1,5 +0,0 @@
-let numberOfDays = Math.ceil((Math.random()*10)*(Math.random()*10));
-numberOfDays += " ";
-numberOfDays += (Math.random()>0.5) ? "month(s)" : "year(s)";
-let text = document.getElementById("noOfDays");
-text.innerHTML = numberOfDays;
